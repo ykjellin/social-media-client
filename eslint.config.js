c9c5c9f3b322c8e6e16f721ebb8f4d98c6eb560d
ykjellin <127968935+ykjellin@users.noreply.github.com>
2024-08-17 15:12:12 +0200

    config cypress

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,39 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import cypressPlugin from "eslint-plugin-cypress";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
+  {
+    plugins: {
+      cypress: cypressPlugin,
+    },
+    rules: {
+      ...cypressPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    ignores: [".cypress/*", ".cypress.config.js"],
+  },
+  {
+    files: ["cypress.config.js"],
+    rules: {
+      "no-unused-vars": "off",
+    },
+  },
+  {
+    files: ["**/*.cy.js"],
+    languageOptions: {
+      globals: { ...globals.cypress },
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "no-undef": "off",
+    },
+  },
 ];
