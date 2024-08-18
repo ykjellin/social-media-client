518f870cf1cba261a53be3732b2ae42bd7d7bbc1
ykjellin <127968935+ykjellin@users.noreply.github.com>
2024-08-18 15:06:14 +0200

    remove force from invalidLogin test. Works locally

--- cypress/e2e/1-login/invalidLogin.cy.js
+++ cypress/e2e/1-login/invalidLogin.cy.js
@@ -3,40 +3,38 @@ describe("Invalid Login Tests", () => {
     cy.fixture("invalidlogin").as("invalidUserData");
     cy.visit("/");
 
-    cy.contains("button", "Login").click({ force: true });
+    cy.contains("button", "Login").click();
   });
 
   it("should not allow login with an invalid email format", () => {
-    cy.get("#loginEmail").type("invalidEmail", { force: true });
-    cy.get("#loginPassword").type("validPassword", { force: true });
+    cy.get("#loginEmail").type("invalidEmail");
+    cy.get("#loginPassword").type("validPassword");
 
-    cy.get('#loginForm button[type="submit"]').click({ force: true });
+    cy.get('#loginForm button[type="submit"]').click();
 
     cy.get("#loginModal").should("be.visible");
   });
 
   it("should not allow login with an incorrect email", function () {
-    cy.get("#loginEmail").type(this.invalidUserData.email, { force: true });
-    cy.get("#loginPassword").type("validPassword", { force: true });
+    cy.get("#loginEmail").type(this.invalidUserData.email);
+    cy.get("#loginPassword").type("validPassword");
 
-    cy.get('#loginForm button[type="submit"]').click({ force: true });
+    cy.get('#loginForm button[type="submit"]').click();
 
     cy.get("#loginModal").should("be.visible");
   });
 
   it("should not allow login with an incorrect password", function () {
-    cy.get("#loginEmail").type("validEmail@noroff.no", { force: true });
-    cy.get("#loginPassword").type(this.invalidUserData.password, {
-      force: true,
-    });
+    cy.get("#loginEmail").type("validEmail@noroff.no");
+    cy.get("#loginPassword").type(this.invalidUserData.password);
 
-    cy.get('#loginForm button[type="submit"]').click({ force: true });
+    cy.get('#loginForm button[type="submit"]').click();
 
     cy.get("#loginModal").should("be.visible");
   });
 
   it("should not allow login when both fields are empty", () => {
-    cy.get('#loginForm button[type="submit"]').click({ force: true });
+    cy.get('#loginForm button[type="submit"]').click();
 
     cy.get("#loginModal").should("be.visible");
   });
